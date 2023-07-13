1cb5174d27b04b333cb8da2daee6a3935dddb544
chaechaeyeon <00dreamadream@kakao.com>
2023-07-14 02:24:58 +0900

    로그인, 회원가입 기능 구현

---
+++ mandalaart/mandalaart/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path , include
+from accounts import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.login, name='login'),
+    path('accounts/',include('accounts.urls',namespace='accounts')),
+]
